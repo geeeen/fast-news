b6a300e74d9612acb53cde585f50edc53577ac0f
Yauheni_Kiaura <Bitbucket31798525>
2020-01-20 14:51:09 +0300

    Build

---
+++ docs/precache-manifest.1928ed9688fa644671a86a9b256858eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3496bdc6ef067dbe4c7d2c08e79d7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25fa5f7d6e5bfa483ac2",
+    "url": "./static/js/2.a75ebfed.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "./static/js/2.a75ebfed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c72c5b6d889d9ab11bb3",
+    "url": "./static/js/main.f805133a.chunk.js"
+  },
+  {
+    "revision": "5ec0053f52a69816a098",
+    "url": "./static/js/runtime-main.2f0fc54a.js"
+  },
+  {
+    "revision": "0c8d302e7243b92e360d483b468f3b09",
+    "url": "./static/media/350-50-30.0c8d302e.png"
+  },
+  {
+    "revision": "4060f2f44267484b6106f62af1070d05",
+    "url": "./static/media/bg.4060f2f4.png"
+  },
+  {
+    "revision": "34f25b66bad10a754b7f0b2db5a90774",
+    "url": "./static/media/cat.34f25b66.png"
+  },
+  {
+    "revision": "06b3760caf2aae2bb4cabaf2f1a0fd36",
+    "url": "./static/media/newspaper.06b3760c.jpg"
+  }
+]);
