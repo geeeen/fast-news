9b8856645495c78c2f500fa5288d9ee4370dc9b7
Yauheni_Kiaura <Bitbucket31798525>
2020-01-20 12:22:59 +0300

    Build

---
+++ build/precache-manifest.c8445eba27fa54a6e17fc37fc756f06c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8496c598cbf6149a608d17ba3897e37",
+    "url": "/fast-news/index.html"
+  },
+  {
+    "revision": "25fa5f7d6e5bfa483ac2",
+    "url": "/fast-news/static/js/2.a75ebfed.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/fast-news/static/js/2.a75ebfed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbc11334f91ddf3667a6",
+    "url": "/fast-news/static/js/main.1861d9d0.chunk.js"
+  },
+  {
+    "revision": "ef885c3b9a3c09fe2b4d",
+    "url": "/fast-news/static/js/runtime-main.f47055af.js"
+  },
+  {
+    "revision": "0c8d302e7243b92e360d483b468f3b09",
+    "url": "/fast-news/static/media/350-50-30.0c8d302e.png"
+  },
+  {
+    "revision": "4060f2f44267484b6106f62af1070d05",
+    "url": "/fast-news/static/media/bg.4060f2f4.png"
+  },
+  {
+    "revision": "34f25b66bad10a754b7f0b2db5a90774",
+    "url": "/fast-news/static/media/cat.34f25b66.png"
+  },
+  {
+    "revision": "06b3760caf2aae2bb4cabaf2f1a0fd36",
+    "url": "/fast-news/static/media/newspaper.06b3760c.jpg"
+  }
+]);
